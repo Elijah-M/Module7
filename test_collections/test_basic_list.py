6362b365715a2d29ec6d68d7c10517e73f8c8ea5
Elijah Morishita <elijah.lm@gmail.com>
2020-10-05 18:21:02 -0500

    topic 1, 2nd commit, Test OK

--- test_collections/test_basic_list.py
+++ test_collections/test_basic_list.py
@@ -1,11 +1,11 @@
 import unittest
-from fun_with_collections import basic_list
+from fun_with_collections.basic_list import make_list
 from unittest.mock import patch
 
 class TestList(unittest.TestCase):
-    @patch('fun_with_collections.get_input', return_value='1')
+    @patch('fun_with_collections.basic_list.get_input', return_value='5.0')
     def test_make_list(self, input):
-        self.assertEqual(basic_list(), [5, 5, 5])
+        self.assertEqual(make_list(), ['5.0', '5.0', '5.0'])
 
 if __name__ == '__main__':
     unittest.self
