e5c7c87015fb9692d5cf2c84278fdc598e948a8b
Elijah Morishita <elijah.lm@gmail.com>
2020-10-05 20:45:28 -0500

    topic 1, 6th commit SEARCH OK

--- test_collections/test_sort_and_search_list.py
+++ test_collections/test_sort_and_search_list.py
@@ -4,7 +4,7 @@ from fun_with_collections.sort_and_search_list import make_list
 from unittest.mock import patch
 
 class TestList(unittest.TestCase):
-    @patch('fun_with_collections.sort_and_search_list.search_list', return_value=2)
+    @patch('fun_with_collections.sort_and_search_list.search_list', return_value=3)
     def test_search_list(self, input):
         x = make_list()
         self.assertEqual(search_list(x), 3)
