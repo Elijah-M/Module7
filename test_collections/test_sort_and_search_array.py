6558a6c2f9c5737fb6e513438f3fdf6bed7b8483
Elijah Morishita <elijah.lm@gmail.com>
2020-10-07 18:27:20 -0500

    topic 4, 12th commit - Search Array OK

--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -5,10 +5,10 @@ from fun_with_collections.sort_and_search_array import sort_array
 from unittest.mock import patch
 
 class Testarray(unittest.TestCase):
-    @patch('fun_with_collections.sort_and_search_array.search_array', return_value=3)
+    @patch('fun_with_collections.sort_and_search_array.search_array', return_value=2)
     def test_search_array(self, input):
         x = make_array()
-        self.assertEqual(search_array(x), 3)
+        self.assertEqual(search_array(x), 2)
 """
     @patch('fun_with_collections.sort_and_search_array.sort_array', return_value=[3.0,45.0,6.0])
     def test_sort_array(self, input):
