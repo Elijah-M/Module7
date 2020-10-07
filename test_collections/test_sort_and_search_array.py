8674bbe226b33cbea6d966119b79712eb2d6334d
Elijah Morishita <elijah.lm@gmail.com>
2020-10-07 18:29:23 -0500

    topic 4, 14th commit - Sort Array OK

--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -10,10 +10,10 @@ class Testarray(unittest.TestCase):
         x = make_array()
         self.assertEqual(search_array(x), 2)
 
-    @patch('fun_with_collections.sort_and_search_array.sort_array', return_value=[3.0,45.0,6.0])
+    @patch('fun_with_collections.sort_and_search_array.sort_array', return_value=[3.0, 45.0, 6.0])
     def test_sort_array(self, input):
         x = make_array()
-        self.assertEqual(sort_array(x), [3.0,6.0, 45.0])
+        self.assertEqual(sort_array(x), [3.0, 6.0, 45.0])
 
 if __name__ == '__main__':
     unittest.self
